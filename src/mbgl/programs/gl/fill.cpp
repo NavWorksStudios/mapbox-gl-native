c5beae640e59e520668791fc04c2665a809a70bd
notheorem <notheorem@hotmail.com>
2024-04-14 09:52:17 +0800

    fill shader

--- src/mbgl/programs/gl/fill.cpp
+++ src/mbgl/programs/gl/fill.cpp
@@ -72,6 +72,9 @@ R"(
 
 attribute vec2 a_pos;
 uniform mat4 u_matrix;
+
+uniform highp float u_base;
+
 #ifndef HAS_UNIFORM_u_color
 uniform lowp float u_color_t;
 attribute highp vec4 a_color;
@@ -79,6 +82,7 @@ varying highp vec4 color;
 #else
 uniform highp vec4 u_color;
 #endif
+
 #ifndef HAS_UNIFORM_u_opacity
 uniform lowp float u_opacity_t;
 attribute lowp vec2 a_opacity;
@@ -86,19 +90,22 @@ varying lowp float opacity;
 #else
 uniform lowp float u_opacity;
 #endif
+
 void main() {
 #ifndef HAS_UNIFORM_u_color
-color=unpack_mix_color(a_color,u_color_t);
+    color=unpack_mix_color(a_color,u_color_t);
 #else
-highp vec4 color=u_color;
+    highp vec4 color=u_color;
 #endif
-#ifndef HAS_UNIFORM_u_opacity
-opacity=unpack_mix_vec2(a_opacity,u_opacity_t);
-#else
-lowp float opacity=u_opacity;
-#endif
-gl_Position=u_matrix*vec4(a_pos,0,1);}
 
+#ifndef HAS_UNIFORM_u_opacity
+    opacity=unpack_mix_vec2(a_opacity,u_opacity_t);
+#else
+    lowp float opacity=u_opacity;
+#endif
+
+    gl_Position=u_matrix*vec4(a_pos,-3,1);
+}
 
 )"; }
     
