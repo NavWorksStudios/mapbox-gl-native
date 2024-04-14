5ea1335aa0877dc06d8485d43e28f164c0996d77
notheorem <notheorem@hotmail.com>
2024-04-14 09:41:37 +0800

    fill shader

--- src/mbgl/programs/gl/fill.cpp
+++ src/mbgl/programs/gl/fill.cpp
@@ -19,7 +19,7 @@ struct ShaderSource<FillProgram> {
     static constexpr const auto vertexOffset = 12862;
     static constexpr const auto fragmentOffset = 13506;
 
-    static const char* navVertex(const char* , size_t ) { return 
+    static const char* navVertex(const char* , size_t ) { return
 R"(
 
 #ifdef GL_ES
@@ -35,22 +35,54 @@ precision highp float;
 #define highp
 #endif
 #endif
-vec2 unpack_float(const float packedValue) {int packedIntValue=int(packedValue);int v0=packedIntValue/256;return vec2(v0,packedIntValue-v0*256);}vec2 unpack_opacity(const float packedOpacity) {int intOpacity=int(packedOpacity)/2;return vec2(float(intOpacity)/127.0,mod(packedOpacity,2.0));}vec4 decode_color(const vec2 encodedColor) {return vec4(unpack_float(encodedColor[0])/255.0,unpack_float(encodedColor[1])/255.0
-);}float unpack_mix_vec2(const vec2 packedValue,const float t) {return mix(packedValue[0],packedValue[1],t);}vec4 unpack_mix_color(const vec4 packedColors,const float t) {vec4 minColor=decode_color(vec2(packedColors[0],packedColors[1]));vec4 maxColor=decode_color(vec2(packedColors[2],packedColors[3]));return mix(minColor,maxColor,t);}vec2 get_pattern_pos(const vec2 pixel_coord_upper,const vec2 pixel_coord_lower,const vec2 pattern_size,const float tile_units_to_pixels,const vec2 pos) {vec2 offset=mod(mod(mod(pixel_coord_upper,pattern_size)*256.0,pattern_size)*256.0+pixel_coord_lower,pattern_size);return (tile_units_to_pixels*pos+offset)/pattern_size;}
+vec2 unpack_float(const float packedValue) {
+    int packedIntValue=int(packedValue);
+    int v0=packedIntValue/256;
+    return vec2(v0,packedIntValue-v0*256);
+}
+
+vec2 unpack_opacity(const float packedOpacity) {
+    int intOpacity=int(packedOpacity)/2;
+    return vec2(float(intOpacity)/127.0,mod(packedOpacity,2.0));
+}
+
+vec4 decode_color(const vec2 encodedColor) {
+    return vec4(unpack_float(encodedColor[0])/255.0,unpack_float(encodedColor[1])/255.0);
+}
+
+float unpack_mix_vec2(const vec2 packedValue,const float t) {
+    return mix(packedValue[0],packedValue[1],t);
+}
+
+vec4 unpack_mix_color(const vec4 packedColors,const float t) {
+    vec4 minColor=decode_color(vec2(packedColors[0],packedColors[1]));
+    vec4 maxColor=decode_color(vec2(packedColors[2],packedColors[3]));
+    return mix(minColor,maxColor,t);
+}
+
+vec2 get_pattern_pos(const vec2 pixel_coord_upper,const vec2 pixel_coord_lower,const vec2 pattern_size,const float tile_units_to_pixels,const vec2 pos) {
+    vec2 offset=mod(mod(mod(pixel_coord_upper,pattern_size)*256.0,pattern_size)*256.0+pixel_coord_lower,pattern_size);
+    return (tile_units_to_pixels*pos+offset)/pattern_size;
+}
 
 )"; }
     
     static const char* navVertex(const char* ) { return 
 R"(
 
-attribute vec2 a_pos;uniform mat4 u_matrix;
+attribute vec2 a_pos;
+uniform mat4 u_matrix;
 #ifndef HAS_UNIFORM_u_color
-uniform lowp float u_color_t;attribute highp vec4 a_color;varying highp vec4 color;
+uniform lowp float u_color_t;
+attribute highp vec4 a_color;
+varying highp vec4 color;
 #else
 uniform highp vec4 u_color;
 #endif
 #ifndef HAS_UNIFORM_u_opacity
-uniform lowp float u_opacity_t;attribute lowp vec2 a_opacity;varying lowp float opacity;
+uniform lowp float u_opacity_t;
+attribute lowp vec2 a_opacity;
+varying lowp float opacity;
 #else
 uniform lowp float u_opacity;
 #endif
@@ -66,6 +98,7 @@ opacity=unpack_mix_vec2(a_opacity,u_opacity_t);
 lowp float opacity=u_opacity;
 #endif
 gl_Position=u_matrix*vec4(a_pos,0,1);}
+
 
 )"; }
     
