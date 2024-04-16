2843ceaa3fdf0bdcaaea808c2e71875e0053997a
notheorem <notheorem@hotmail.com>
2024-04-16 22:01:01 +0800

    layer filter

--- src/mbgl/nav/nav_mb_layer.cpp
+++ src/mbgl/nav/nav_mb_layer.cpp
@@ -20,24 +20,21 @@ bool filterLayerByType(const std::string& id) {
 #if 1
 //        {"land", "", /* background */ },  地球底色，灰黄色
         
-        {"water", "water", /* fill */ }, // 所有水系，江河湖海
-        
         {ID_NAV_LAND, ID_NAV_LAND, /* fill */ }, // nav 陆地底座
 
-        {"landcover", "landcover", /* fill */ }, // 大洲绿色，低级别才显示
+        {"landcover", "landcover", /* fill */ }, // 覆盖物，大洲绿色，低级别才显示
         {"national-park", "landuse_overlay", /* fill */ }, // 国家公园，绿地
         
-        {"landuse", "landuse", /* fill */ }, // 用地："park", "airport", "cemetery", "glacier", "hospital", "pitch", "sand", "school", 
+        {"landuse", "landuse", /* fill */ }, // 用地："park", "airport", "cemetery", "glacier", "hospital", "pitch", "sand", "school",
 //        {"pitch-outline", "landuse", /* line */ }, // 球场轮廓
 
 //        {"water-shadow", "water", /* fill */ }, // 水边阴影
 //        {"waterway", "waterway", /* line */ }, // 航道
         
-
+        {"water", "water", /* fill */ }, // 所有水系，江河湖海
+        
 //        {"hillshade", "hillshade", /* fill */ }, // 山阴
-#endif
 
-#if 0
 //        {"land-structure-polygon", "structure", /* fill */ },
 //        {"land-structure-line", "structure", /* line */ },
         
@@ -45,16 +42,16 @@ bool filterLayerByType(const std::string& id) {
 //        {"aeroway-line", "aeroway", /* line */ }, // 航路
         
 //        {"building-outline", "building", /* line */ }, // 楼底座的沟边
-        {"building", "building", /* fill */ }, // 楼底座
+//        {"building", "building", /* fill */ }, // 楼底座
 #endif
         
-#if 0
-        {"tunnel-street-minor-low", "road", /* line */ },
-        {"tunnel-street-minor-case", "road", /* line */ },
-        {"tunnel-primary-secondary-tertiary-case", "road", /* line */ },
-        {"tunnel-major-link-case", "road", /* line */ },
-        {"tunnel-motorway-trunk-case", "road", /* line */ },
-        {"tunnel-construction", "road", /* line */ },
+#if 1
+//        {"tunnel-street-minor-low", "road", /* line */ },
+//        {"tunnel-street-minor-case", "road", /* line */ },
+//        {"tunnel-primary-secondary-tertiary-case", "road", /* line */ },
+//        {"tunnel-major-link-case", "road", /* line */ },
+//        {"tunnel-motorway-trunk-case", "road", /* line */ },
+//        {"tunnel-construction", "road", /* line */ },
         {"tunnel-path", "road", /* line */ },
         {"tunnel-steps", "road", /* line */ },
         {"tunnel-major-link", "road", /* line */ },
@@ -71,29 +68,30 @@ bool filterLayerByType(const std::string& id) {
         {"ferry-auto", "road", /* line */ }, // 摆渡，水上，实线
 #endif
         
-#if 0
-        {"road-path-bg", "road", /* line */ }, // 固定宽度道路
-        {"road-steps-bg", "road", /* line */ }, // 道路台阶
-        {"turning-feature-outline", "road", /* symbol */ }, // 类似环岛的圆形，灰色，被turning-feature压盖
-        {"road-pedestrian-case", "road", /* line */ }, // 人行道-case
-        {"road-minor-low", "road", /* line */ }, // 次要道路-low
+#if 1
+//        {"road-path-bg", "road", /* line */ }, // 固定宽度道路
+//        {"road-steps-bg", "road", /* line */ }, // 道路台阶
+//        {"turning-feature-outline", "road", /* symbol */ }, // 类似环岛的圆形，灰色，被turning-feature压盖
+//        {"road-pedestrian-case", "road", /* line */ }, // 人行道-case
+//        {"road-minor-low", "road", /* line */ }, // 次要道路-low
+//        
+//        {"road-street-low", "road", /* line */ }, // 公路街道-low
+//        {"road-minor-case", "road", /* line */ }, // 次要道路-case
+//        {"road-street-case", "road", /* line */ }, // 公路街道-case
+//        {"road-secondary-tertiary-case", "road", /* line */ }, // 公路二级-三级-case
+//        {"road-primary-case", "road", /* line */ }, // 主要公路-case
+//
+//        {"road-major-link-case", "road", /* line */ }, // 公路主干道-case
+//        {"road-motorway-trunk-case", "road", /* line */ }, // 公路机动车道干线-case
+//        {"road-construction", "road", /* line */ }, // 在建道路
         
-        {"road-street-low", "road", /* line */ }, // 公路街道-low
-        {"road-minor-case", "road", /* line */ }, // 次要道路-case
-        {"road-street-case", "road", /* line */ }, // 公路街道-case
-        {"road-secondary-tertiary-case", "road", /* line */ }, // 公路二级-三级-case
-        {"road-primary-case", "road", /* line */ }, // 主要公路-case
-
-        {"road-major-link-case", "road", /* line */ }, // 公路主干道-case
-        {"road-motorway-trunk-case", "road", /* line */ }, // 公路机动车道干线-case
-        {"road-construction", "road", /* line */ }, // 道路施工
         {"road-path", "road", /* line */ }, // 道路
         {"road-steps", "road", /* line */ }, // 道路台阶
 
         {"road-major-link", "road", /* line */ }, // 公路主干道
-//        {"road-pedestrian", "road", /* line */ }, // 人行道
-//        {"road-pedestrian-polygon-fill", "road", /* fill */ }, // 人行道多边形填充
-//        {"road-pedestrian-polygon-pattern", "road", /* fill */ }, // 人行道多边形图案
+        {"road-pedestrian", "road", /* line */ }, // 人行道
+        {"road-pedestrian-polygon-fill", "road", /* fill */ }, // 人行道多边形填充
+        {"road-pedestrian-polygon-pattern", "road", /* fill */ }, // 人行道多边形图案
         {"road-polygon", "road", /* fill */ }, // 道路多边形
         
 
@@ -116,16 +114,17 @@ bool filterLayerByType(const std::string& id) {
 //        {"golf-hole-line", "road", /* line */ }, // 高尔夫球洞线
         
         
-#if 0   // 不开
-        {"bridge-path-bg", "road", /* line */ },
-        {"bridge-steps-bg", "road", /* line */ },
-        {"bridge-pedestrian-case", "road", /* line */ },
-        {"bridge-street-minor-low", "road", /* line */ },
-        {"bridge-street-minor-case", "road", /* line */ },
-        {"bridge-primary-secondary-tertiary-case", "road", /* line */ },
-        {"bridge-major-link-case", "road", /* line */ },
-        {"bridge-motorway-trunk-case", "road", /* line */ },
-        {"bridge-construction", "road", /* line */ },
+#if 1
+//        {"bridge-path-bg", "road", /* line */ },
+//        {"bridge-steps-bg", "road", /* line */ },
+//        {"bridge-pedestrian-case", "road", /* line */ },
+//        {"bridge-street-minor-low", "road", /* line */ },
+//        {"bridge-street-minor-case", "road", /* line */ },
+//        {"bridge-primary-secondary-tertiary-case", "road", /* line */ },
+//        {"bridge-major-link-case", "road", /* line */ },
+//        {"bridge-motorway-trunk-case", "road", /* line */ },
+//        {"bridge-construction", "road", /* line */ },
+        
         {"bridge-path", "road", /* line */ },
         {"bridge-steps", "road", /* line */ },
         {"bridge-major-link", "road", /* line */ },
@@ -147,7 +146,7 @@ bool filterLayerByType(const std::string& id) {
 //        {"aerialway", "road", /* line */ },
         
 
-#if 0
+#if 1
         {"admin-1-boundary-bg", "admin", /* line */ }, //省界
         {"admin-0-boundary-bg", "admin", /* line */ }, // 国界
         {"admin-1-boundary", "admin", /* line */ }, //省界
@@ -155,11 +154,11 @@ bool filterLayerByType(const std::string& id) {
         {"admin-0-boundary-disputed", "admin", /* line */ }, // 争议国界
 #endif
         
-#if 0
+#if 1
         {"building-number-label", "housenum_label", /* symbol */ },
-//        {"road-label", "road", /* symbol */ },
-//        {"road-number-shield", "road", /* symbol */ },
-//        {"road-exit-shield", "motorway_junction", /* symbol */ },
+        {"road-label", "road", /* symbol */ },
+        {"road-number-shield", "road", /* symbol */ },
+        {"road-exit-shield", "motorway_junction", /* symbol */ },
 //        {"golf-hole-label", "road", /* symbol */ },
 //        {"waterway-label", "natural_label", /* symbol */ },
 //        {"natural-line-label", "natural_label", /* symbol */ },
