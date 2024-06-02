e91a726ecee56b5423d7da80d4a0b1b7f09179a6
“bernie zhao” <notheorem@hotmail.com>
2024-06-02 09:23:31 +0800

    rm bridge log

--- src/mbgl/nav/nav_unity_bridge.cpp
+++ src/mbgl/nav/nav_unity_bridge.cpp
@@ -19,12 +19,12 @@ namespace projection {
 MatrixObserver notifyMatrixObserver = nullptr;
 void setMatrixObserver(MatrixObserver observer) { notifyMatrixObserver = observer; }
 void onMatrix(const double* matrix) {
-    nav::log::i("Projection",
-                "matrix : [%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf]\n",
-                matrix[0], matrix[1], matrix[2], matrix[3],
-                matrix[4], matrix[5], matrix[6], matrix[7],
-                matrix[8], matrix[9], matrix[10], matrix[11],
-                matrix[12], matrix[13], matrix[14], matrix[15]);
+//    nav::log::i("Projection",
+//                "matrix : [%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf]\n",
+//                matrix[0], matrix[1], matrix[2], matrix[3],
+//                matrix[4], matrix[5], matrix[6], matrix[7],
+//                matrix[8], matrix[9], matrix[10], matrix[11],
+//                matrix[12], matrix[13], matrix[14], matrix[15]);
     
     if (notifyMatrixObserver) {
         notifyMatrixObserver(matrix);
@@ -34,10 +34,10 @@ void onMatrix(const double* matrix) {
 TransformObserver notifyTransformObserver = nullptr;
 void setTransformObserver(TransformObserver observer) { notifyTransformObserver = observer; }
 void onTransform(const double* position, const double* quaternion) {
-    nav::log::i("Projection",
-                "transform : t[%lf,%lf,%lf] q[%lf,%lf,%lf,%lf]\n",
-                position[0], position[1], position[2],
-                quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
+//    nav::log::i("Projection",
+//                "transform : t[%lf,%lf,%lf] q[%lf,%lf,%lf,%lf]\n",
+//                position[0], position[1], position[2],
+//                quaternion[0], quaternion[1], quaternion[2], quaternion[3]);
 
     if (notifyTransformObserver) {
         notifyTransformObserver(position, quaternion);
@@ -52,13 +52,13 @@ namespace model {
 TileMatrixObserver notifyTileMatrixObserver = nullptr;
 void setTileMatrixObserver(TileMatrixObserver observer) { notifyTileMatrixObserver = observer; }
 void onTileMatrix(const TileId* tileId, const double* matrix) {
-    nav::log::i("Model",
-                "matrix : (%d,%d,%d) [%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf]\n",
-                tileId->x, tileId->y, tileId->z,
-                matrix[0], matrix[1], matrix[2], matrix[3],
-                matrix[4], matrix[5], matrix[6], matrix[7],
-                matrix[8], matrix[9], matrix[10], matrix[11],
-                matrix[12], matrix[13], matrix[14], matrix[15]);
+//    nav::log::i("Model",
+//                "matrix : (%d,%d,%d) [%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf][%lf,%lf,%lf,%lf]\n",
+//                tileId->x, tileId->y, tileId->z,
+//                matrix[0], matrix[1], matrix[2], matrix[3],
+//                matrix[4], matrix[5], matrix[6], matrix[7],
+//                matrix[8], matrix[9], matrix[10], matrix[11],
+//                matrix[12], matrix[13], matrix[14], matrix[15]);
     
     if (notifyTileMatrixObserver) {
         notifyTileMatrixObserver(tileId, matrix);
@@ -68,11 +68,11 @@ void onTileMatrix(const TileId* tileId, const double* matrix) {
 TileTransformObserver notifyTileTransformObserver = nullptr;
 void setTileTransformObserver(TileTransformObserver observer) { notifyTileTransformObserver = observer; }
 void onTileTransform(const TileId* tileId, const double* position, const double* scale) {
-    nav::log::i("Model",
-                "transform : (%d,%d,%d) T[%lf,%lf,%lf] S[%lf,%lf,%lf]\n",
-                tileId->x, tileId->y, tileId->z,
-                position[0], position[1], position[2],
-                scale[0], scale[1], scale[2]);
+//    nav::log::i("Model",
+//                "transform : (%d,%d,%d) T[%lf,%lf,%lf] S[%lf,%lf,%lf]\n",
+//                tileId->x, tileId->y, tileId->z,
+//                position[0], position[1], position[2],
+//                scale[0], scale[1], scale[2]);
     
     if (notifyTileTransformObserver) {
         notifyTileTransformObserver(tileId, position, scale);
