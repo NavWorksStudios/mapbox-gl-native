ce1145271333e286480080b484af94a9454c39c3
notheorem <notheorem@hotmail.com>
2024-07-13 17:25:24 +0800

    nothing

--- src/mbgl/programs/gl/fill_extrusion.cpp
+++ src/mbgl/programs/gl/fill_extrusion.cpp
@@ -89,7 +89,6 @@ struct ShaderSource<FillExtrusionProgram> {
         attribute highp vec2 a_pos;
         attribute lowp vec4 a_normal_ed;
     
-        varying lowp vec3 v_pos;
         varying lowp vec4 v_color;
 
         varying lowp float v_top_edge;
@@ -162,9 +161,7 @@ struct ShaderSource<FillExtrusionProgram> {
                 gl_Position.x=1.e100;
                 return;
             }
-    
-            v_pos=gl_Position.xyz;
-    
+
             // ----------------------------- vertex color -----------------------------
 
             // directional light
@@ -213,14 +210,14 @@ struct ShaderSource<FillExtrusionProgram> {
             v_bottom_edge=8./tall;
             v_height=h?1.:0.;
     
-            // 楼顶镜面反射
-            const vec3 cameraPos = vec3(0.,500.,0.);
-            const vec3 lightPos = vec3(0.,3000.,1500.);
-            const lowp float specular = 1.; // 镜面强度
-            const lowp float shininess = 1.; // 反射率
-            vec3 lightDir=normalize(lightPos-v_pos);
-            vec3 viewDir=normalize(cameraPos-v_pos);
-            vec3 reflectDir=reflect(-lightDir, vec3(0.,1.,0.)); // reflect (genType I, genType N),返回反射向量
+            // 镜面反射
+            const vec3 cameraPos=vec3(0.,500.,0.);
+            const vec3 lightPos=vec3(0.,3000.,1500.);
+            const lowp float specular=1.; // 镜面强度
+            const lowp float shininess=1.; // 反射率
+            lowp vec3 lightDir=normalize(lightPos-gl_Position.xyz);
+            lowp vec3 viewDir=normalize(cameraPos-gl_Position.xyz);
+            lowp vec3 reflectDir=reflect(-lightDir,vec3(0.,1.,0.)); // reflect (genType I, genType N),返回反射向量
             v_specular=specular*pow(max(dot(viewDir,reflectDir),0.0),shininess); // power(max(0,dot(N,H)),shininess)
     
             // 距离屏幕中心点越近，越透明
@@ -261,7 +258,6 @@ struct ShaderSource<FillExtrusionProgram> {
         uniform bool u_render_reflection;
         uniform lowp float u_render_time;
 
-        varying lowp vec3 v_pos;
         varying lowp vec4 v_color;
 
         varying lowp float v_top_edge;
@@ -271,22 +267,22 @@ struct ShaderSource<FillExtrusionProgram> {
         varying lowp float v_centerFactor;
 
         void main() {
-            lowp float lighten;
+            lowp float brighten;
             if (v_height>.9999) {
                 // 楼顶 镜面反射
-                lighten = .5 + v_specular;
+                brighten = .5 + v_specular;
             } else if (v_height>v_top_edge) {
                 // 上边缘 发光
-                lighten = (v_height-v_top_edge) / (1.-v_top_edge);
+                brighten = (v_height-v_top_edge) / (1.-v_top_edge);
             } else if (v_height<v_bottom_edge) {
                 // 下边缘 发光
-                lighten = (v_bottom_edge-v_height) / v_bottom_edge;
+                brighten = (v_bottom_edge-v_height) / v_bottom_edge;
             } else {
-                lighten = (1. - v_centerFactor) * .5;
+                brighten = (1. - v_centerFactor) * .5;
             }
-            lighten = pow(lighten,3.);
+            brighten = pow(brighten,3.);
 
-            gl_FragColor.rgb = v_color.rgb * (lighten*.5 + v_centerFactor*.5);
+            gl_FragColor.rgb = v_color.rgb * (brighten*.5 + v_centerFactor*.5);
             gl_FragColor.a = v_color.a * v_centerFactor;
         
         #ifdef OVERDRAW_INSPECTOR
