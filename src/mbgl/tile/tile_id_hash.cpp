36cb75a36e3776bcb293956d1d893905b90625e7
notheorem <notheorem@hotmail.com>
2024-07-21 15:21:18 +0800

    简化tile id hash

--- src/mbgl/tile/tile_id_hash.cpp
+++ src/mbgl/tile/tile_id_hash.cpp
@@ -4,24 +4,42 @@
 namespace std {
 
 size_t hash<mbgl::CanonicalTileID>::operator()(const mbgl::CanonicalTileID& id) const {
-    std::size_t seed = 0;
-    mbgl::util::hash_combine(seed, id.x);
-    mbgl::util::hash_combine(seed, id.y);
-    mbgl::util::hash_combine(seed, id.z);
+//    std::size_t seed = 0;
+//    mbgl::util::hash_combine(seed, id.x);
+//    mbgl::util::hash_combine(seed, id.y);
+//    mbgl::util::hash_combine(seed, id.z);
+//    return seed;
+    
+    std::size_t seed;
+    uint32_t* p = (uint32_t*) &seed;
+    p[0] = id.x + id.z;
+    p[1] = id.y;
     return seed;
 }
 
 size_t hash<mbgl::UnwrappedTileID>::operator()(const mbgl::UnwrappedTileID& id) const {
-    std::size_t seed = 0;
-    mbgl::util::hash_combine(seed, std::hash<mbgl::CanonicalTileID>{}(id.canonical));
-    mbgl::util::hash_combine(seed, id.wrap);
+//    std::size_t seed = 0;
+//    mbgl::util::hash_combine(seed, std::hash<mbgl::CanonicalTileID>{}(id.canonical));
+//    mbgl::util::hash_combine(seed, id.wrap);
+//    return seed;
+    
+    std::size_t seed;
+    uint32_t* p = (uint32_t*) &seed;
+    p[0] = id.canonical.x + id.canonical.z;
+    p[1] = id.canonical.y + id.wrap;
     return seed;
 }
 
 size_t hash<mbgl::OverscaledTileID>::operator()(const mbgl::OverscaledTileID& id) const {
-    std::size_t seed = 0;
-    mbgl::util::hash_combine(seed, std::hash<mbgl::CanonicalTileID>{}(id.canonical));
-    mbgl::util::hash_combine(seed, id.overscaledZ);
+//    std::size_t seed = 0;
+//    mbgl::util::hash_combine(seed, std::hash<mbgl::CanonicalTileID>{}(id.canonical));
+//    mbgl::util::hash_combine(seed, id.overscaledZ);
+//    return seed;
+    
+    std::size_t seed;
+    uint32_t* p = (uint32_t*) &seed;
+    p[0] = id.canonical.x + id.canonical.z + id.overscaledZ;
+    p[1] = id.canonical.y + id.wrap;
     return seed;
 }
 
