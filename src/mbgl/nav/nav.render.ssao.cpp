8b86acda365c837c506bcbb148a534c9053337f5
notheorem <notheorem@hotmail.com>
2024-09-09 14:59:52 +0800

    注释

--- src/mbgl/nav/nav.render.ssao.cpp
+++ src/mbgl/nav/nav.render.ssao.cpp
@@ -451,9 +451,15 @@ void drawModel(bool ssao)
         glUniform3fv(phongProgKSpc, 1, floorKSpc);
         glUniform1f(phongProgKShn, floorKShn);
         
+        // 绑定一个顶点缓冲对象（VBO）。floorBuf是特定的缓冲区对象标识符。这使得后续的操作都针对这个特定的缓冲区进行。
         glBindBuffer(GL_ARRAY_BUFFER, floorBuf);
+        // 定义顶点属性指针。phongProgPosAttrib是顶点位置属性的索引。3表示该顶点属性有三个分量，可能对应三维空间中的坐标（x、y、z）。
+        // GL_FLOAT是顶点属性的数据类型为浮点数。GL_FALSE表示不进行归一化。
+        // 6 * sizeof(GLfloat)是步长，即两个连续顶点属性之间的字节数。这里表示每六个浮点数（每个浮点数大小为sizeof(GLfloat)）为一个完整的顶点数据。
+        // reinterpret_cast<void*>(0)表示位置属性在缓冲区中的偏移量为 0，即从缓冲区起始位置开始读取位置数据。
         glVertexAttribPointer(phongProgPosAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<void*>(0));
         glVertexAttribPointer(phongProgNormAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<void*>(3*sizeof(GLfloat)));
+        // 使用顶点数组数据进行绘制。GL_TRIANGLES表示使用三角形的方式进行绘制。0是起始顶点的索引。6表示要绘制的顶点数量。这意味着将绘制两个三角形，因为每个三角形需要三个顶点。
         glDrawArrays(GL_TRIANGLES, 0, 6);
     }
 
@@ -482,31 +488,47 @@ void doSSAO()
     
     // switch frame buffer
     {
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer); // 往 framebuffer 上画
+        // 绑定一个帧缓冲对象。framebuffer是一个特定的帧缓冲对象标识符。这使得后续的操作都针对这个特定的帧缓冲对象进行。
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        // 设置绘制缓冲区为颜色附件 0。这指定了渲染结果将被写入到帧缓冲对象的哪个颜色附件中。
         glDrawBuffer(GL_COLOR_ATTACHMENT0);
         
+        // 将一个二维纹理（aoTexture）附着到当前绑定的帧缓冲对象的颜色附件 0 上。这样，渲染结果将被写入到这个纹理中。
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, aoTexture, 0);
+        
+        // 对于颜色附件 1，不附着任何纹理。这里将其设置为 0，表示没有纹理与之关联。
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, 0, 0);
+        
         // Detach any depth textures
+        // 从帧缓冲对象的深度附件中分离任何已附着的二维纹理。这确保深度测试使用的是渲染缓冲区而不是纹理。
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, 0, 0);
+        // 将一个渲染缓冲区（depthRenderbuffer）附着到帧缓冲对象的深度附件上。这通常用于存储深度信息，以便进行深度测试和遮挡剔除。
         glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
     }
     
+    // 清除颜色缓冲区和深度缓冲区
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glUseProgram(aoProg);
     
     // bind texture
     {
+        // 激活纹理单元 0。
         glActiveTexture(GL_TEXTURE0);
+        // 激活纹理单元 0。在 OpenGL 中，可以同时使用多个纹理单元，每个纹理单元可以绑定不同的纹理。这里激活了第一个纹理单元。
         glBindTexture(GL_TEXTURE_2D, depthTexture);
         
+        // 激活纹理单元 1。
         glActiveTexture(GL_TEXTURE1);
+        // 将名为normalTexture的二维纹理绑定到纹理单元 1。这样在渲染时可以根据需要切换到这个纹理来获取法线信息。
         glBindTexture(GL_TEXTURE_2D, normalTexture);
         
+        // 激活纹理单元 2。
         glActiveTexture(GL_TEXTURE2);
+        // 将名为randomTexture的二维纹理绑定到纹理单元 2。这个纹理可能用于提供随机数据或其他特定的用途。
         glBindTexture(GL_TEXTURE_2D, randomTexture);
         
+        // 最后再次激活纹理单元 0。这可能是为了确保在后续的操作中，主要的纹理单元处于激活状态，或者是为了方便后续对第一个纹理的操作。
         glActiveTexture(GL_TEXTURE0);
     }
 
@@ -537,9 +559,11 @@ void doSSAO()
             1.0f, -1.0f,
             -1.0f, -1.0f };
         
+        // 启用顶点属性数组。
         glEnableVertexAttribArray(aoProgPosAttrib);
-        
+        // 绑定顶点缓冲对象（VBO）。这里将GL_ARRAY_BUFFER绑定到索引为 0 的缓冲区，通常意味着解除之前绑定的任何顶点缓冲对象。
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // 定义顶点属性指针。2表示该顶点属性的大小（这里表示有两个分量，可能是二维坐标）。GL_FLOAT是顶点属性的数据类型。 GL_FALSE表示是否进行归一化。0表示顶点属性之间的偏移量。
         glVertexAttribPointer(aoProgPosAttrib, 2, GL_FLOAT, GL_FALSE, 0, verts);
         
         // 纹理贴图 GL_TRIANGLE_STRIP 规则比较复杂
@@ -552,16 +576,17 @@ void doSSAO()
 
 void doBlur()
 {
-    // switch frame buffer
+    // switch frame buffer to draw
     {
-        glBindFramebuffer(GL_FRAMEBUFFER, 0); // Actually render to the screen
+        // Actually render to the screen
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glUseProgram(blurProg);
     
-    // bind texture
+    // bind texture aoTexture & colorTexture
     {
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, aoTexture);
@@ -572,7 +597,7 @@ void doBlur()
         glActiveTexture(GL_TEXTURE0);
     }
     
-    // set uniform
+    // set uniform values
     {
         glUniform1i(blurProgAoTexture, 0);
         glUniform1i(blurProgColorTexture, 1);
@@ -588,10 +613,10 @@ void doBlur()
             -1.0f, -1.0f };
         
         glEnableVertexAttribArray(blurProgPosAttrib);
-        
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glVertexAttribPointer(blurProgPosAttrib, 2, GL_FLOAT, GL_FALSE, 0, verts);
         
+        // 纹理贴图 GL_TRIANGLE_STRIP 规则比较复杂
         // index (0,1,2) (2,1,3)
         // { (1,1),(-1,1),(1,-1) }, { (1,-1),(-1,1),(-1,-1) }
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
