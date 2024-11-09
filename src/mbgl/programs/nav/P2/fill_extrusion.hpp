5767f79d19fdfb1db9f4e38ef07351b9d18d201c
notheorem <notheorem@hotmail.com>
2024-11-09 10:43:14 +0800

    楼块调光

--- src/mbgl/programs/nav/P2/fill_extrusion.hpp
+++ src/mbgl/programs/nav/P2/fill_extrusion.hpp
@@ -90,10 +90,10 @@ struct FillExtrusionProgram {
     
             vec3 fragPos = vec3(u_model_matrix * pos);
     
-            const float Material_ambient = .4; // 环境光
-            const float Material_diffuse = .4; // 漫反射
-            const float Material_specular = 2.; // 镜面反射
-            const float Material_shininess = 1.; // 反射率
+            const float Material_ambient = .45; // 环境光
+            const float Material_diffuse = .5; // 漫反射
+            const float Material_specular = 3.; // 镜面反射
+            const float Material_shininess = .8; // 反射率
 
             // Ambient Lighting
             const float ambient = Material_ambient;
@@ -107,12 +107,13 @@ struct FillExtrusionProgram {
             vec3 viewDir = normalize(u_camera_pos - fragPos);
             vec3 reflectDir = reflect(-lightDir, norm);
             float specular = pow(max(dot(viewDir, reflectDir), 0.), Material_shininess) * Material_specular;
+            specular = min(specular, 1.);
 
-            vec3 baselight = vec3(.78, .8, .8) * (ambient + diffuse) * max(1. - specular, 0.);
+            vec3 baselight = vec3(.92, .9, .88) * (ambient + diffuse) * (1. - specular);
             vec3 specularlight = vec3(.98, .88, .78) * specular;
     
             v_color = vec4(baselight + specularlight, 1.) * u_opacity;
-            v_color.a *= .6;
+            v_color.a *= .8;
         }
         
     )"; }
